f6441c6c8afd5527f893f998bd846dd263a46d4c
Ariflutfi <arif.kurniawan@students.amikom.ac.id>
2023-01-10 14:40:53 +0700

    waktu habis

---
+++ app/src/main/java/com/responsi/pcs/Login.kt
@@ -0,0 +1,24 @@
+package com.responsi.pcs
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+
+        actionBar?.hide();
+
+
+
+
+
+
+
+
+
+    }
+
+
+}
